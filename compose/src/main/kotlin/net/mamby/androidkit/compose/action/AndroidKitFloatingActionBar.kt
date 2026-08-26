845922a94564a5b0268027eef9919e587500ada8
Mamby Camara <mamby@live.com>
2026-08-26 09:11:17 +0200

    up

--- compose/src/main/kotlin/net/mamby/androidkit/compose/action/AndroidKitFloatingActionBar.kt
+++ compose/src/main/kotlin/net/mamby/androidkit/compose/action/AndroidKitFloatingActionBar.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -318,10 +317,9 @@ private fun FloatingActionBarItemContent(
     val dimensions = AndroidKitThemeTokens.dimensions
     Column(
         modifier = modifier
-            .heightIn(min = dimensions.minimumTouchTarget)
-            .minimumInteractiveComponentSize()
             .clip(AndroidKitThemeTokens.shapes.extraLarge)
             .clickable(role = Role.Button, onClick = onClick)
+            .minimumInteractiveComponentSize()
             .padding(horizontal = dimensions.spaceSmall),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(dimensions.spaceExtraSmall),
